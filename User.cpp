c25b4cbee95347f75105ddde1a0026a24701d511
leni <99428447+leni666@users.noreply.github.com>
2024-06-23 03:58:52 +0300

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,21 +1,23 @@
 #include "User.h"
 
-User::User(MyString name, size_t id, size_t age, MyString password) : id(id), age(age)
+User::User(const MyString& name, const MyString& id, size_t age, MyString password) : data(name, id, age, password)
 {
-	this->name = name;
-	this->password = password;
 }
 
 void User::whoAmI() const
 {
-	std::cout << "You are " << name;
+	std::cout << "You are " << data.getName();
 }
 
+const MyString& User::getName() const
+{
+	return data.getName();
+}
 
 
 bool User::isValidUser(MyString name, MyString password) const
 {
-	if (this->name == name && this->password == password)
+	if (data.getName() == name && data.getPassword() == password)
 	{
 		return true;
 	}
