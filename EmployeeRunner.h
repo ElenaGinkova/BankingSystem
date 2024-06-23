c429851c82a18c74734f54f9825f112e6dad8faf
leni <99428447+leni666@users.noreply.github.com>
2024-06-23 04:00:15 +0300

    Update EmployeeRunner.h

--- EmployeeRunner.h
+++ EmployeeRunner.h
@@ -2,11 +2,28 @@
 #include "BankSystem.h"
 class EmployeeRunner
 {
-private:
-	void help() const;
 public:
-	void runEmployee(BankSystem* system);
+	EmployeeRunner(BankSystem& system);
+	void runEmployee();
+	void approve(Employee& employee);
+	void disapprove(Employee& employee);
+	void validateChange(Employee& employee);
+	void help() const;
+
+private:
+	BankSystem& system;
+
+	void approveChange(Employee& employee, size_t taskNum);
+	void approveOpen(Employee& employee, size_t taskNum);
+	void approveClose(Employee& employee, size_t taskNum);
+
+	Client& getClient(Task* task) const;
+	void sendCustomMessage(MyString mess, Client& client, Employee& employee, size_t taskNum);
+	void changeBanks(const MyString&, const MyString&, size_t accIndx, Account* acc);
+
+
+	void getCustomMessage(MyString& message);
+	static const size_t MAX_BUFF_SIZE;
 };
 
-//think...
-//tooks care of running the interface
+//excecute, send messege, pop 
