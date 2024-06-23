eb30ed952ebd7c3b277889de003778c87ba7370a
leni <99428447+leni666@users.noreply.github.com>
2024-06-23 03:57:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
 #include "EmployeeRunner.h"
-//
-//void excecute(BankingSystem& bankingSystem)
-//{
-//    switch (bankingSystem.getUserType())
-//    {
-//         //case userType::Client:// EmployeeRunner::runEmployee(&bankingSystem); break;
-//         // case userType::Employee: //clientEmployee(bankingSystem); break;
-//    //     //  case userType::ThridPartyEmployee: //clientThridPartyEmployee(bankingSystem); break;
-//    }
-//
-//}
-//
-//void run(BankingSystem& bankingSystem)
-//{
-//    MyString command;
-//    while (!bankingSystem.getLoggedIn() && command == "exit")
-//    {
-//        std::cin >> command;
-//        if (command == "login")
-//        {
-//            bankingSystem.logIn();
-//            excecute(bankingSystem);
-//        }
-//        else if (command == "signup")
-//        {
-//            bankingSystem.signUp();
-//        }
-//        else if (command == "create_bank")
-//        {
-//            bankingSystem.createBank();
-//        }
-//    }
-//}
+#include "ClientRunner.h"
+#include "ThirdPartyEmployeeRunner.h"
 
+void runBankSystem(BankSystem& bankSystem)
+{
+    static EmployeeRunner employeeRunner(bankSystem);
+    static ClientRunner clientRunner(bankSystem);
+    static ThirdPartyEmployeeRunner thirdPartyEmployeeRunner(bankSystem);
+
+    switch (bankSystem.getUserType())
+    {
+    case UserType::EmployeeType:  employeeRunner.runEmployee(); break;
+    case UserType::ClientType: clientRunner.runClient(); break;
+    case UserType::ThridPartyEmployeeType: thirdPartyEmployeeRunner; break;
+    }
+
+}
+
+void run(BankSystem& bankSystem)
+{
+    MyString command;
+    while (command != "exit")
+    {
+        std::cin >> command;
+        if (command == "login")
+        {
+            bankSystem.logIn();
+            runBankSystem(bankSystem);
+        }
+        else if (command == "signup")
+        {
+            bankSystem.signUp();
+        }
+        else if (command == "create_bank")
+        {
+            bankSystem.createBank();
+        }
+        else
+        {
+            std::cout << "No such command!";
+        }
+    }
+}
 
 int main()
 {
-  /*  BankingSystem bankingSystem;
-    run(bankingSystem);*/
+   BankSystem bankSystem;
+   run(bankSystem);
 }
