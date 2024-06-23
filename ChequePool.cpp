bdb6a5ebb168473e30c2fb83d32c45d8e784f2f0
leni <99428447+leni666@users.noreply.github.com>
2024-06-23 04:01:10 +0300

    Update ChequePool.cpp

--- ChequePool.cpp
+++ ChequePool.cpp
@@ -1,1 +1,64 @@
 #include "ChequePool.h"
+#include <iostream>
+#include <exception>
+void ChequePool::resize()
+{
+	capacity *= 2;
+	ChequeCode* newData = new ChequeCode[capacity];
+	for (int i = 0; i < size; i++)
+	{
+		newData[i] = usedCodes[i];
+	}
+	delete[] usedCodes;
+	usedCodes = newData;
+}
+
+void ChequePool::free()
+{
+	delete[] usedCodes;
+}
+
+bool ChequePool::chequeExists(const char* cheque) const
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (usedCodes[i].getData() == cheque)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+const ChequeCode& ChequePool::allocate(const char* cheque, double sum)
+{
+	if (size == capacity)
+	{
+		resize();
+	}
+	usedCodes[size] = ChequeCode(cheque, sum);
+	return usedCodes[size++];
+}
+
+ChequePool::ChequePool()
+{
+	usedCodes = new ChequeCode[capacity];
+}
+
+const ChequeCode& ChequePool::getAllocatedCode(const char* cheque, double sum)
+{
+	if (std::strlen(cheque) != 3)
+	{
+		throw std::logic_error("Invalid cheque");
+	}
+	if (chequeExists(cheque))
+	{
+		throw std::runtime_error("Cheque exists");
+	}
+	return allocate(cheque, sum);
+}
+
+ChequePool::~ChequePool()
+{
+	free();
+}
