f4aa00e82a62dcf6c581fb30db4b6eef74a7d9fa
leni <99428447+leni666@users.noreply.github.com>
2024-06-24 15:25:48 +0300

    Update Cheque.cpp

--- Cheque.cpp
+++ Cheque.cpp
@@ -1,7 +1,14 @@
 #include "Cheque.h"
+#include <exception>
+#include <iostream>
+#include <fstream>
 
 ChequeCode::ChequeCode(const char* data, double sum)
 {
+	if (!validate(data))
+	{
+		throw std::runtime_error("Invalid check code");
+	}
 	for (int i = 0; i < CHEQUE_LEN; i++)
 	{
 		this->data[i] = data[i];
@@ -12,4 +19,38 @@ ChequeCode::ChequeCode(const char* data, double sum)
 const char* ChequeCode::getData() const
 {
 	return data;
+}
+
+double ChequeCode::getSum() const
+{
+	return sum;
+}
+
+void ChequeCode::saveToFile(std::ofstream& ofs)
+{
+	ofs.write((const char*)(data), sizeof(char) * CHEQUE_LEN);
+	ofs.write((const char*)(&sum), sizeof(sum));
+}
+
+void ChequeCode::readFromFile(std::ifstream& ifs)
+{
+	ifs.read((char*)(data), sizeof(char) * CHEQUE_LEN);
+	ifs.read((char*)(&sum), sizeof(sum));
+}
+
+bool ChequeCode::isValidChar(char ch) const
+{
+	return ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
+}
+
+bool ChequeCode::validate(const char* data) const
+{
+	for (int i = 0; i < CHEQUE_LEN - 1; i++)
+	{
+		if (!isValidChar(data[i]))
+		{
+			return false;
+		}
+	}
+	return true;
 }
