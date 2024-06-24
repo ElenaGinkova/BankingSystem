93163204e0c6c03dc6467057ecadb34bebc0d037
leni <99428447+leni666@users.noreply.github.com>
2024-06-24 15:14:38 +0300

    Update ChangeTask.h

--- ChangeTask.h
+++ ChangeTask.h
@@ -11,19 +11,17 @@ enum class TaskStatus
 class ChangeTask : public Task
 {
 public:
-	ChangeTask(const MyString& oldBank, const MyString& newBank, Account* acc, UserInfo data, TaskType type);
+	ChangeTask(const MyString& oldBank, const MyString& newBank, Account& acc, UserInfo data, TaskType type);
 	void viewInfo() const override;
-	Account* getAccount()const;
+	Account& getAccount()const;
 	const MyString& getOldBank();
 	const MyString& getNewBank();
 	void setStatus(TaskStatus);
-	//void setIsDataCorrect(bool);
 	TaskStatus getStatus()const;
 	void change();
-	//bool getIsDataCorrect()const;
 	Task* clone() const override;
 private:
-	Account* acc;
+	Account& acc;
 	MyString oldBank;
 	MyString newBank;
 	TaskStatus status = TaskStatus::NeedsApproval;
