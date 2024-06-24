eb8c282724fb7836f44f88e8aa3c4b4b82716130
leni <99428447+leni666@users.noreply.github.com>
2024-06-24 15:16:58 +0300

    Update EmployeeRunner.h

--- EmployeeRunner.h
+++ EmployeeRunner.h
@@ -5,25 +5,25 @@ class EmployeeRunner
 public:
 	EmployeeRunner(BankSystem& system);
 	void runEmployee();
-	void approve(Employee& employee);
-	void disapprove(Employee& employee);
-	void validateChange(Employee& employee);
+	void approve();
+	void disapprove();
+	void validateChange();
 	void help() const;
 
 private:
 	BankSystem& system;
+	Employee* employee = nullptr;
 
-	void approveChange(Employee& employee, size_t taskNum);
-	void approveOpen(Employee& employee, size_t taskNum);
-	void approveClose(Employee& employee, size_t taskNum);
+	void approveChange(size_t taskNum);
+	void approveOpen(size_t taskNum);
+	void approveClose(size_t taskNum);
 
 	Client& getClient(Task* task) const;
-	void sendCustomMessage(MyString mess, Client& client, Employee& employee, size_t taskNum);
-	void changeBanks(const MyString&, const MyString&, size_t accIndx, Account* acc);
+	void sendCustomMessage(MyString mess, Client& client, size_t taskNum);
+	void changeBanks(const MyString&, const MyString&, size_t accIndx, Account& acc);
 
 
 	void getCustomMessage(MyString& message);
 	static const size_t MAX_BUFF_SIZE;
+	int taskIndex();
 };
-
-//excecute, send messege, pop 
