ebae7a31c0980ac555b4932b1d5c1b68af16fb6b
leni <99428447+leni666@users.noreply.github.com>
2024-06-24 15:14:56 +0300

    Update Cheque.h

--- Cheque.h
+++ Cheque.h
@@ -1,11 +1,19 @@
 #pragma once
+#include <fstream>
 struct ChequeCode
 {
 	ChequeCode() = default;
 	ChequeCode(const char* data, double sum);
 	const char* getData()const;
+	double getSum()const;
+
+	void saveToFile(std::ofstream& ofs);
+	void readFromFile(std::ifstream& ifs);
 private:
-	static const size_t CHEQUE_LEN = 3;
+	bool isValidChar(char ch)const;
+	bool validate(const char*)const;
+	static const size_t CHEQUE_LEN = 4;
+
 	char data[CHEQUE_LEN]{};
 	double sum = 0.0;
 };
