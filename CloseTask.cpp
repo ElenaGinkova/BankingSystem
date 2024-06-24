c472d099f65c6a0e6d1e39abcc296c884f0316d3
leni <99428447+leni666@users.noreply.github.com>
2024-06-24 15:24:35 +0300

    Update CloseTask.cpp

--- CloseTask.cpp
+++ CloseTask.cpp
@@ -1,8 +1,7 @@
 #include "CloseTask.h"
 
-CloseTask::CloseTask(UserInfo data, Account* acc, TaskType type) : Task(data, type)
+CloseTask::CloseTask(UserInfo data, Account& acc, TaskType type) : Task(data, type), acc(acc)
 {
-	this->acc = acc;
 	this->taskName = "Close - ";
 	message += data.getName();
 	message += " wants to close an account";
@@ -12,13 +11,19 @@ void CloseTask::viewInfo() const
 {
 	std::cout << taskName << " request from:\n";
 	data.printInfo();
-	acc->printInfo();
+	acc.printInfo();
 }
-Account* CloseTask::getAccount() const
+Account& CloseTask::getAccount() const
 {
 	return acc;
 }
 Task* CloseTask::clone() const
 {
 	return new CloseTask(*this);
+}
+
+void CloseTask::saveToFile(std::ofstream& ofs)
+{
+	size_t accID = acc.getAccId();
+	ofs.write((const char*)(&accID), sizeof(size_t));
 }
