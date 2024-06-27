12f96ab46c485e25cb9278f5a0015c5641e95f0b
Elena Ginkova <99428447+leni666@users.noreply.github.com>
2024-06-27 19:20:53 +0300

    Update ThirdPartyEmployeeRunner.h

--- ThirdPartyEmployeeRunner.h
+++ ThirdPartyEmployeeRunner.h
@@ -1,12 +1,13 @@
 #pragma once
+#include "Runner.h"
 #include "ThirdPartyEmployeeRunner.h"
 #include "BankSystem.h"
-class ThirdPartyEmployeeRunner
+class ThirdPartyEmployeeRunner : public Runner
 {
 public:
 	ThirdPartyEmployeeRunner(BankSystem& system);
 
-	void runThirdPartyEmployee();
+	void run() override;
 private:
 	BankSystem& system;
 
