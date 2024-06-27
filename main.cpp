c07211dc79ef3ca14102b14e90e1b72115aa9c57
Elena Ginkova <99428447+leni666@users.noreply.github.com>
2024-06-27 17:19:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,9 @@
 #include <iostream>
-#include "EmployeeRunner.h"
-#include "ClientRunner.h"
-#include "ThirdPartyEmployeeRunner.h"
-
-void runBankSystem(BankSystem& bankSystem)
-{
-    static EmployeeRunner employeeRunner(bankSystem);
-    static ClientRunner clientRunner(bankSystem);
-    static ThirdPartyEmployeeRunner thirdPartyEmployeeRunner(bankSystem);
-
-    switch (bankSystem.getUserType())
-    {
-    case UserType::EmployeeType:  employeeRunner.runEmployee(); break;
-    case UserType::ClientType: clientRunner.runClient(); break;
-    case UserType::ThridPartyEmployeeType: thirdPartyEmployeeRunner.runThirdPartyEmployee(); break;
-    }
-
-}
-
-void excecute(BankSystem& bankSystem)
-{
-    MyString command;
-    while (true)
-    {
-        std::cin >> command;
-        if (command == "login")
-        {
-            bankSystem.logIn();
-            runBankSystem(bankSystem);
-        }
-        else if (command == "signup")
-        {
-            bankSystem.signUp();
-        }
-        else if (command == "create_bank")
-        {
-            bankSystem.createBank();
-        }
-        else if (command == "exit")
-        {
-            bankSystem.saveToFile();
-            break;
-        }
-        else
-        {
-            std::cout << "No such command!";
-        }
-    }
-}
+#include "Excecuter.h"
 
 int main()
 {
    BankSystem bankSystem;
    bankSystem.readFromFile();
-   excecute(bankSystem);
+   Excecuter::excecute(bankSystem);
 }
